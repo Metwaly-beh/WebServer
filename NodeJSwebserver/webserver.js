3e9e564637373e7a17c3c31cb94e95ded72d6b25
Metwaly-beh <adhammetwaly25@gmail.com>
2026-04-25 10:05:37 +0300

    finished simple webserver

--- NodeJSwebserver/webserver.js
+++ NodeJSwebserver/webserver.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const fs = require('fs');
 
 
 
@@ -7,9 +8,33 @@ const server= net.createServer((socket)=>{
     socket.on("data",(buffer)=>{
         const requestString =  buffer.toString("utf-8")
 
-        console.log(requestString);
+        const request = parseRequest(requestString)
+
+        console.log(request.method, request.path, request.protocol);
+
+        if(request.method=== "GET"){
+            if (fs.existsSync('.${request.path}')){
+
+
+            
+            socket.write("HTTP/1.0 200 OK");
+            }
+            else{
+                socket.write("HTTP/1.0 404 Not Found");
+            }
+            
+        }
+
     })
 })
 
+const parseRequest = (requestString)=> {
+    const [method, path, protocol]= requestString.split(" ");
+    return{
+        method,
+        path,
+        protocol
 
+    }
+}
 server.listen(9999,()=> console.log("listening"))
